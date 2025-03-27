798a31f1eba263d1bb612908a7fda5aba33baa6f
Togara Hess <togarahess@gmail.com>
2025-03-27 02:18:51 +0100

    Lnding page texts updated

--- saaspro/app/page.tsx
+++ saaspro/app/page.tsx
@@ -16,12 +16,12 @@ export default function Home() {
   return (
     <>
       <header className="sticky top-0 z-10 bg-background p-4 border-b-2 border-slate-200 dark:border-slate-800 flex flex-row justify-between items-center">
-        Convex + Next.js + Clerk
+        Thrice Logo Button
         <UserButton />
       </header>
       <main className="p-8 flex flex-col gap-8">
         <h1 className="text-4xl font-bold text-center">
-          Convex + Next.js + Clerk
+          Togara Codes (Thrice Logo)
         </h1>
         <Authenticated>
           <Content />
