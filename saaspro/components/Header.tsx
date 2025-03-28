6e1407099eb91b4311a588af4fdd62fec0cca308
Togara Hess <togarahess@gmail.com>
2025-03-28 10:56:05 +0100

    missing closing div added on line 27

--- saaspro/components/Header.tsx
+++ saaspro/components/Header.tsx
@@ -1,4 +1,5 @@
 "use client"
+
 import { SignedIn, SignInButton, SignedOut, UserButton } from "@clerk/nextjs"
 import { Shield } from "lucide-react"
 import Link from "next/link"
@@ -9,8 +10,11 @@ function Header() {
     <div className="p-4 flex justify-between items-center">
     <Link href="/" className="flex items-center">
         <Shield className="w-6 h-6 text-blue-600 mr-2" />
+
         <h1 className="text-xl font-semibold">Togara SaaS App</h1>
     </Link>
+
+<div className="flex items-center space-x-4">
     <SignedIn>
         <UserButton />
     </SignedIn>
@@ -20,6 +24,8 @@ function Header() {
         </SignInButton>
     </SignedOut>
     </div>
+    </div>
+    
   )
 }
 
