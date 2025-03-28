77f50353fbfaaacbc29b6114248256d541aaa9e2
Togara Hess <togarahess@gmail.com>
2025-03-28 10:11:25 +0100

    Shield Icon imported from Lucid in Header component

--- saaspro/components/Header.tsx
+++ saaspro/components/Header.tsx
@@ -1,11 +1,15 @@
-import React from 'react'
-import { Button } from './ui/button'
+"use client"
+import { Shield } from "lucide-react"
+import Link from "next/link"
 
 function Header() {
   return (
-    <div className=''>
-    <div>Header</div>
-    <Button>Hello</Button>
+    <div className="">
+    <Link href="/">
+        <Shield className="w-6 h-6 text-blue-600 mr-2" />
+        <h1 className="text-xl font-semibold">Togara SaaS App</h1>
+    </Link>
+    
     </div>
   )
 }
