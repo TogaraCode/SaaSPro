ca2598ce4942cddf5e97675b88f628ec0d9176ef
Togara Hess <togarahess@gmail.com>
2025-03-28 10:41:22 +0100

    SignedIn and out components added to header from Clerk

--- saaspro/components/Header.tsx
+++ saaspro/components/Header.tsx
@@ -1,15 +1,24 @@
 "use client"
+import { SignedIn, SignInButton, SignedOut, UserButton } from "@clerk/nextjs"
 import { Shield } from "lucide-react"
 import Link from "next/link"
+import { Button } from "./ui/button"
 
 function Header() {
   return (
-    <div className="">
-    <Link href="/">
+    <div className="p-4 flex justify-between items-center">
+    <Link href="/" className="flex items-center">
         <Shield className="w-6 h-6 text-blue-600 mr-2" />
         <h1 className="text-xl font-semibold">Togara SaaS App</h1>
     </Link>
-    
+    <SignedIn>
+        <UserButton />
+    </SignedIn>
+    <SignedOut>
+        <SignInButton>
+            <Button>Login</Button>
+        </SignInButton>
+    </SignedOut>
     </div>
   )
 }
